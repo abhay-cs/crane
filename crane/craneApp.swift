2a27357f97a60b66d4a1f1c6d5b44e2447aa4555
Abhay Sharma <abhay9729188142@gmail.com>
2026-05-17 23:27:53 -0500

    Initial Commit

---
+++ crane/craneApp.swift
@@ -0,0 +1,17 @@
+//
+//  craneApp.swift
+//  crane
+//
+//  Created by Abhay Sharma on 2026-05-17.
+//
+
+import SwiftUI
+
+@main
+struct craneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
